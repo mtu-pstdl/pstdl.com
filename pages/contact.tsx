3a5bc53805d251d7fc10c0582569a37f53961ead
Elijah Cobb <ejcobb@mtu.edu>
2021-06-15 13:53:45 -0400

    added links

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -20,12 +20,12 @@ export default function ContactPage(): ReactElement {
 			<li>Email Dr. Paul van Susante at <a className={"link"} href={"mailto:pjvansus@mtu.edu"}>pjvansus@mtu.edu</a> with notification of completion of the application form and introduction.</li>
 			<li>Applications will be reviewed on a rolling basis.</li>
 		</ol>
-		<a href={"mailto:pjvansus@mtu.edu"} className={"button"}>Apply</a>
+		<a target={"_blank"} rel={"noreferrer nofollow"} href={"https://docs.google.com/forms/d/e/1FAIpQLScJdRdlkQuihcYNBQ-kx_Kpb9iJMfBG5oaXuGqo2WZVFzz9Yg/viewform?usp=sf_link"} className={"button"}>Apply</a>
 		<h3>Enterprise Application</h3>
 		<h4>What is Enterprise?</h4>
 		<p>The Michigan Tech Enterprise Program is an integrated curriculum that enables interdisciplinary teams of undergraduates to work on complex engineering projects while earning credits towards their degree. Teams perform testing and analyses, make recommendations, manufacture parts, stay within budgets and schedules, and manage multiple projects while faculty and industry members act as coaches and mentors. Teams are often multidisciplinary, mimicking the project team structure in many industries.</p>
 		<h4>Mining INnovation Enterprise (MINE)</h4>
 		<p>The Mining INnovation Enterprise (MINE) seeks to design, test, and implement mining innovation technologies for industry partners. MINE works in interdisciplinary sub-teams to solve current and future challenges for the mining industry on earth and off-planet. Opportunities include developing autonomous lunar robotic mining missions, undersea mining ROVs, and developing ISRU technology.</p>
-		<a href={"mailto:pjvansus@mtu.edu"} className={"button"}>Apply</a>
+		<a target={"_blank"} rel={"noreferrer nofollow"} href={"https://docs.google.com/forms/d/e/1FAIpQLSf1UO5LW3DbGT7rhxr7L_-646cVdU_hrcQEN-D9HKsAH4jDBw/viewform?usp=sf_link"} className={"button"}>Apply</a>
 	</Layout>
 }
