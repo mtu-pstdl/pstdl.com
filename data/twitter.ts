32cdfec7a0297f8db8bf5121b5ec693896636381
Elijah Cobb <ejcobb@mtu.edu>
2022-03-16 14:18:06 -0400

    changed to env for twitter key

--- data/twitter.ts
+++ data/twitter.ts
@@ -46,8 +46,11 @@ export interface Tweet {
 interface TwitterAuth {consumer_key: string, consumer_secret: string, bearer_token: string}
 
 function fetchCredentials(): TwitterAuth {
-	return JSON.parse(FS.readFileSync("./twitter-auth.json").toString("utf8"));
-
+	return {
+		consumer_key: process.env.TWITTER_KEY,
+		consumer_secret: process.env.TWITTER_SECRET,
+		bearer_token: process.env.TWITTER_TOKEN,
+	}
 }
 
 export async function fetchTweets(): Promise<News[]> {
