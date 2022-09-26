6e4e2544e4d435263b7d105d9a5aeaff746fba90
Elijah Cobb <elijah.cobb@vercel.com>
2022-09-26 17:05:00 -0400

    revamped for react 18

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
-// @ts-check
-
-const path = require('path')
-
-/**
- * @type {import('next/dist/next-server/server/config').NextConfig}
- **/
-const nextConfig = {
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')],
-    },
-    productionBrowserSourceMaps: true
+module.exports = {
+  // next.js config
+  reactStrictMode: true,
 };
-
-module.exports = nextConfig;
