3685d5e36a40ff904074bf5bb469a5149f383f84
Andrew Frey <afrey@mtu.edu>
2023-07-17 21:09:09 -0400

    Added contanct info for renting facilities

--- pages/facilities/index.tsx
+++ pages/facilities/index.tsx
@@ -13,6 +13,7 @@ import {facilities} from "../../data/facilities";
 export default function FacilitiesPage(): ReactElement {
 	return <Layout title={"Facilities"}>
 		<h2>Facilities</h2>
+		<p>Interested in running tests in one of our facilities? Contact <a href="pjvansus@mtu.edu"> Dr Paul van Susante</a></p>
 		<DetailItemPage base={"facilities"} items={facilities}/>
 	</Layout>
 }
