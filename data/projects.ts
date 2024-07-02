d817f5d4c535d0abacbb13185fe7d8b48313a199
MackMiller2 <kenzieskye22@gmail.com>
2024-07-02 10:39:57 -0400

    added new member

--- data/projects.ts
+++ data/projects.ts
@@ -69,6 +69,7 @@ import {
 	ircoulin,
 	audreyal,
 	leifchri,
+	cmdinkel
 } from "./members";
 import {Member} from "../interfaces/Member";
 
@@ -233,7 +234,8 @@ export const breakTheIce: DetailItem = {
 		bjengle,
 		audreyal,
 		gtredlon,
-		leifchri
+		leifchri,
+		cmdinkel
 	],
 	alumni: [],
 	images: [
