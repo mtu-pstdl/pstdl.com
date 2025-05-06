d793a56bdb44f484391d1ad7500abd252e8ae80c
mackmiller22 <kenzieskye22@gmail.com>
2025-05-06 14:38:38 -0400

    Updated project section to divide completed and on going projects

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -11,8 +11,17 @@ import {DetailItemPage} from "../../components/DetailItemPage";
 import {projects} from "../../data/projects";
 
 export default function ProjectsPage(): ReactElement {
+	const completed = projects.filter(p =>
+		p.link === "btic" || p.link === "class"
+	  );
+	  const ongoing = projects.filter(p =>
+		p.link !== "btic" && p.link !== "class"
+	  );
 	return <Layout title={"Projects"}>
 		<h2>Projects</h2>
-		<DetailItemPage base={"projects"} items={projects}/>
+		<h2>Ongoing Projects</h2>
+		<DetailItemPage base={"projects"} items={ongoing}/>
+		<h2>Completed Projects</h2>
+		<DetailItemPage base={"projects"} items={completed}/>
 	</Layout>
 }
